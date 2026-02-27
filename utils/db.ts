0ccfa42b86150f5002f8e5733be0579632656669
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-02-27 17:47:50 +0530

    connecting database

---
+++ utils/db.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const connectionString: string = process.env.MONGODB_URI;
+
+if (!connectionString) {
+    throw new Error("Please provide a MongoDB connection string");
+}
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const existingConnection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if (existingConnection.isConnected) {
+        console.log("Already connected to MongoDB");
+        return;
+    }
+    try {
+        const db = await mongoose.connect(connectionString);
+
+        existingConnection.isConnected = db.connections[0].readyState
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+}
+
+export default dbConnect;

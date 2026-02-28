77289d104e7342e66b705310208cb34c1e9a222f
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-02-28 16:46:57 +0530

    creating trip model

--- models/trip.model.ts
+++ models/trip.model.ts
@@ -1,5 +1,44 @@
 import mongoose from "mongoose";
 
+const taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+    },
+    time: {
+        type: String,
+        required: [true, "Time is required"],
+    },
+    status: {
+        type: String,
+        enum: ["pending", "completed"],
+        default: "pending",
+    },
+    remarks: {
+        type: String,
+        default: "",
+    },
+    submittedAt: {
+        type: String,
+        default: "",
+    },
+
+})
+
+const daysSchema = new mongoose.Schema({
+    date: {
+        type: String,
+        required: [true, "Date is required"],
+    },
+    tasks: [taskSchema],
+})
+
+
+
 const tripSchema = new mongoose.Schema({
     tripName: {
         type: String,
@@ -13,6 +52,9 @@ const tripSchema = new mongoose.Schema({
         type: String,
         required: [true, "Duration is mendatory"]
     },
-
-
+    days: [daysSchema],
 })
+
+const TripModel = mongoose.models.Trip || mongoose.model("Trip", tripSchema);
+
+export default TripModel;

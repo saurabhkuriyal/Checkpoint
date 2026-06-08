e61f6a0a8536e0c2f6fbc857f73a8a2dc7c9d525
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-06-08 13:19:01 +0530

    get tast api tested succesfully

--- app/api/manager/gettask/route.ts
+++ app/api/manager/gettask/route.ts
@@ -7,8 +7,12 @@ export async function GET(req: NextRequest) {
     try {
         await connectDB();
         const tasks = await TaskModel.find();
+        if (!tasks) {
+            return NextResponse.json({ message: "No tasks found", status: 404 });
+        }
         return NextResponse.json({ message: "Tasks fetched successfully", status: 200, tasks });
     } catch (error) {
         console.log("error in getting task", error);
+        return NextResponse.json({ message: "Error in getting task", status: 500, error });
     }
 }

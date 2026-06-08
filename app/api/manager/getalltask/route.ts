ea3184b5ef7cafc6cf2d0cdd707b1927382c1b73
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-06-08 23:36:04 +0530

    creatinh get all task api

---
+++ app/api/manager/getalltask/route.ts
@@ -0,0 +1,19 @@
+import taskModel from "@/models/task.model";
+import connectDB from "@/utils/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const allTasks = await taskModel.find();
+        console.log("alltasks--->", allTasks);
+
+        return NextResponse.json({ message: "tasks fetched successfully", tasks: allTasks, status: 200 });
+
+    }
+    catch (error) {
+        console.log("error in getting all tasks", error);
+        return NextResponse.json({ message: "Error" }, { status: 500 });
+    }
+}

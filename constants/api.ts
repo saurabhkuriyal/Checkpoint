f4b145b87922657193b33d47d2766d86a5c8faf9
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-07-10 11:30:50 +0530

    adding backend api in constants

--- constants/api.ts
+++ constants/api.ts
@@ -4,6 +4,6 @@ export const API = {
     signup: "http://localhost:4000/api/v1/auth/signup",
     login: "http://localhost:4000/api/v1/auth/login",
     // inventory: "http://localhost:4000/api/v1/mess/updatestock"
-    inventory: "http://localhost:3000/api/mess/putstock",
-    getInventory: "http://localhost:3000/api/mess/getStock"
+    inventory: "/api/mess/putstock",
+    getInventory: "/api/mess/getStock"
 }

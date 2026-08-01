f0e4c2a9f5ded7cb8fc68d39a5e40775fb015eef
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-08-01 15:49:27 +0530

    making the pwa

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,41 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
